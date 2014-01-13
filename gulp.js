09029deda6c92f4f6e10db0bb530cb993335614e
Peter de Croos <cultofmetatron@aumlogic.com>
2014-01-12 18:35:05 -0800

    added koa-common

---
+++ gulp.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+
+
+
+gulp.task('default', function() {
+  console.log('running tasks');
+
+
+});
---
+++ test/server.js
@@ -0,0 +1,16 @@
+var koa = require('koa');
+var app = koa();
+
+app.use(function *(next) {
+  var start = new Date;
+  yield next;
+  var ms = new Date() - start
+  console.log('%s %s - %s', this.method, this.url, ms);
+});
+
+
+app.use(function *(){
+  this.body = 'Hello World';
+});
+
+app.listen(3000);
