74df2a004139d01096f801e7546a99c36d339836
Peter de Croos <cultofmetatron@aumlogic.com>
2014-02-04 21:40:25 -0800

    got the cascade function working

--- test/server.js
+++ test/server.js
@@ -4,6 +4,27 @@ var app = koa();
 var Visa = require('../index.js');
 var visa = new Visa();
 var session = require('koa-session');
+var _ = require('underscore');
+
+var cascade = function() {
+  var args = Array.prototype.slice.call(arguments);
+  
+  return function *(next) {
+    console.log('yollo')
+    yield _.reduce(args, function(memo, nextone) {
+      var self = this;
+      return function *(next){
+        console.log('what');
+        console.log('memo: ', memo, 'nextOne: ', nextone);
+        yield memo.call(self, nextone.call(self, next));
+      };
+    }, function *(next) {
+      yield next;
+    }, this).call(this, next);
+  };
+};
+
+
 
 var users = {
   'cultofmetatron': '12345',
@@ -19,9 +40,36 @@ var users = {
 
 app.use(session());
 app.use(visa.mount());
-app.use(visa.authenticate(function* () {
+/*
+app.use(visa.authenticate(
+  function* () {
   console.log('middleware works!!');
+  console.log('this: ', this);
+  this.body = "authentication failed";
+}, function *(next) {
+  console.log('second middleware');
+  yield next;
 }));
+*/
+
+app.use(cascade(
+  function *(next){
+    console.log('1');
+    yield next;
+  },
+  function *(next){
+    console.log('2');
+    yield next;
+  },
+  function *(next){
+    console.log('3');
+    yield next
+  },
+  function *(next){
+    console.log('4');
+    yield next;
+  }
+));
 
 app.use(function *(){
   this.body = 'Hello World';
