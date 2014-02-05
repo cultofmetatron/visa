6ed673295011db75a85f434a8867b4955cf172ac
Peter de Croos <cultofmetatron@aumlogic.com>
2014-02-04 19:07:49 -0800

    got autheticated middleware flow  working

--- test/server.js
+++ test/server.js
@@ -3,23 +3,25 @@ var koa = require('koa');
 var app = koa();
 var Visa = require('../index.js');
 var visa = new Visa();
-var session = require(koa-session);
+var session = require('koa-session');
 
 var users = {
   'cultofmetatron': '12345',
   'hulioMandingo':  '12345'
-}
+};
 
-var LocalStrategy = require('../lib/local.js');
-
-visa.useStrategy('local', new LocalStrategy({
+//var LocalStrategy = require('../lib/local.js');
+//console.log('localize me', LocalStrategy);
+//visa.useStrategy('local', new LocalStrategy({
   
-}));
+//}));
 
 
-app.use(session)
+app.use(session());
 app.use(visa.mount());
-
+app.use(visa.authenticate(function* () {
+  console.log('middleware works!!');
+}));
 
 app.use(function *(){
   this.body = 'Hello World';
